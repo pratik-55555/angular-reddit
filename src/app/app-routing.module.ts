daaf5f95d15dab1be939ae16b2b73600f3afff79
Pratik Chauhan <pratikchauhan55555@gmail.com>
2021-02-08 19:19:25 +0530

    Signup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+   { path: 'sign-up', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
